48b71d3cba26dd7f5bd0bd88e7c3b31aebd1adeb
ramya110704-design <ramya110704@gmail.com>
2026-01-17 12:05:45 +0530

    oper.java file created

---
+++ oper.java
@@ -0,0 +1,18 @@
+public class oper{
+    public static void main(String[] args){
+        int a=20;
+        int b=15;
+        boolean isEqual=(a==b);
+        boolean isNotequal=(a!=b);
+        boolean isGreater=(a>b);
+        boolean isLesser=(a<b);
+        boolean isGreaterorEqual=(a>=b);
+        boolean isLesserorEqual=(a<=b);
+        System.out.println("a==b"+isEqual);
+        System.out.println("a!=b"+isNotequal);
+        System.out.println("a>b"+isGreater);
+        System.out.println("a<b"+isLesser);
+        System.out.println("a>=b"+isGreaterorEqual);
+        System.out.println("a<=b"+isLesserorEqual);
+    }
+}
